a15cb7bdfe0cdf3536804c9ba8e6a593afcdf83e
Songwen Xue <60166898+OwenSit@users.noreply.github.com>
2021-04-02 22:06:35 -0500

    added j_music.sh

---
+++ j_music.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This script use MPV to play Jasmin Cheung's music shuffelly
+# Make sure you have MPV player install beforehand.
+# Premission: regular user
+# Author: Songwen Xue
+
+Jasmin_YT_page="https://www.youtube.com/user/jasminegreen3364"
+mpv --shuffle --no-video $Jasmin_YT_page 
