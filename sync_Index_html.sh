663d8139b29c5c7844dfddf61a98fbcc41e03905
SongwenXue <60166898+OwenSit@users.noreply.github.com>
2021-04-04 12:32:00 -0500

    Update sync_Index_html.sh

--- sync_Index_html.sh
+++ sync_Index_html.sh
@@ -2,3 +2,12 @@
 
 # This script is used to sync the latest update of my index.html used on site songwenx.xyz
 
+# Firstly change the WSL working directory to the songwenx.xyz directory
+PATH_songwenx_xyz="/mnt/e/Document/U/songwenx.xyz"  # Change this var if you need to
+cd $PATH_songwenx_xyz
+
+# Then sync with the remote Nginix server:
+remote_PATH="root@songwenx.xyz:/var/www/songwenx"
+scp -v index.html $remote_PATH
+
+ 
